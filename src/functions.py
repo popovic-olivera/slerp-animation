ad0f3b098b1a208809a39ea5edb91220759092c2
popovic-olivera <olja997@hotmail.com>
2019-12-12 10:19:50 +0100

    Added all files

---
+++ src/functions.py
@@ -0,0 +1,155 @@
+import math
+import sys
+import numpy as np
+import numpy.linalg as la
+
+
+# Rz(psi)*Ry(teta)*Rx(fi)
+def Euler2A(fi, teta, psi):
+    sin_fi = math.sin(fi)
+    cos_fi = math.cos(fi)
+    sin_teta = math.sin(teta)
+    cos_teta = math.cos(teta)
+    sin_psi = math.sin(psi)
+    cos_psi = math.cos(psi)
+
+    return np.array([
+        [cos_teta*cos_psi, cos_psi*sin_teta*sin_fi-cos_fi*sin_psi, cos_fi*cos_psi*sin_teta+sin_fi*sin_psi],
+        [cos_teta*sin_psi, cos_fi*cos_psi+sin_teta*sin_fi*sin_psi, cos_fi*sin_teta*sin_psi-cos_psi*sin_fi],
+        [-sin_teta, cos_teta*sin_fi, cos_teta*cos_fi]
+    ])
+
+
+def A2AxisAngle(A):
+    if (A == np.eye(3)).all() or round(la.det(A)) != 1:
+        sys.exit("A2AxisAngle: Matrica A je jedinicna ili joj je determinanta razlicita od 1...")
+
+    # Odredjujemo jedinicni sopstveni vektor za lambda = 1
+    # A*p = lambda*p => 0 = (A - E)p
+
+    A_p = A - np.eye(3)
+
+    u = []
+    p = []
+
+    # p ce biti jednak vektorskom proizvodu dve ne nula vrste matrice A_p
+    # (jer p treba da bude normalan na sve vrste, sto se vidi iz toga da je skalarni proizvod = 0)
+    # posto u treba da bude normalan na p, moze se uzeti bilo koja ne nula vrsta matrice A_p
+
+    if not np.array_equal(np.round(np.cross(A_p[0], A_p[1]), 3), np.zeros((3,))):
+        p = np.cross(A_p[0], A_p[1])
+        u = np.array(A_p[0])
+    elif not np.array_equal(np.round(np.cross(A_p[1], A_p[2]), 3), np.zeros((3,))):
+        p = np.cross(A_p[1], A_p[2])
+        u = np.array(A_p[1])
+    elif not np.array_equal(np.round(np.cross(A_p[2], A_p[0]), 3), np.zeros((3,))):
+        p = np.cross(A_p[2], A_p[0])
+        u = np.array(A_p[2])
+
+    # postavimo da p i u budu jedinicni
+    norm_p = np.linalg.norm(p)
+    if norm_p != 0:
+        p = p / norm_p
+
+    norm = np.linalg.norm(u)
+    if norm != 0:
+        u = u / norm
+
+    # ne mora se proveravati da li je jedinicni jer je A izometrija
+    u_prim = A.dot(u)
+
+    alpha = math.acos(u.dot(u_prim))
+
+    # proveravamo da bi rotacija bila u pozitivnom smeru
+    if (np.cross(u, u_prim)).dot(p) < 0:
+        p = -p
+
+    return p, alpha
+
+
+# dobija se matrica rotacije za ugao alpha oko orijentisane prave p
+def Rodrigez(p, alpha):
+    if round(la.norm(p)) != 1:
+        norm_p = la.norm(p)
+        if norm_p != 0:
+            p = p / norm_p
+
+    # Formula Rodrigeza:
+    # Rp(fi) = p*p_t + cos(fi)*(E - p*p_t) + sin(fi)*px
+
+    pp_t = np.outer(p, p)
+    cos_alpha = math.cos(alpha)
+    sin_alpha = math.sin(alpha)
+
+    px = np.array([
+        [0, -p[2], p[1]],
+        [p[2], 0, -p[0]],
+        [-p[1], p[0], 0]
+    ])
+
+    return pp_t + cos_alpha*(np.eye(3) - pp_t) + sin_alpha*px
+
+
+def A2Euler(A):
+    if round(la.det(A)) != 1:
+        sys.exit("Matrica B nije ortogonalna...")
+
+    A = np.array(A)
+    if A[2, 0] < 1:
+        if A[2, 0] > -1:
+            psi = math.atan2(A[1, 0], A[0, 0])
+            delta = math.asin(-A[2, 0])
+            fi = math.atan2(A[2, 1], A[2, 2])
+        else:   # nije jedinstveno resenje
+            psi = math.atan2(-A[0, 1], A[1, 1])
+            delta = math.pi / 2     # A[2, 0] = sin(delta) = 1 tj. delta = pi/2
+            fi = 0
+    else:   # nije jedinstveno resenje
+        psi = math.atan2(-A[0, 1], A[1, 1])
+        delta = -math.pi / 2    # A[2, 0] = -sin(delta) = 1 tj. delta = -pi/2
+        fi = 0
+
+    return fi, delta, psi
+
+
+# svaki q = [vector*sin(alpha), cos(alpha)] je svetska rotacija oko vector za 2*alpha
+def AngleAxis2Q(p, fi):
+    if fi == 0:
+        return 1
+
+    w = math.cos(fi / 2)
+
+    norm_p = la.norm(p)
+    if norm_p != 0:
+        p = p / norm_p
+
+    x, y, z = math.sin(fi / 2) * p
+
+    return [x, y, z, w]
+
+
+def Q2AxisAngle(q):
+    norm_q = la.norm(q)
+    if norm_q != 0:
+        q = q / norm_q
+
+    w = q[3]
+
+    # ako zelimo da fi bude iz [0, pi]
+    if w < 0:
+        q = -q
+
+    fi = 2 * math.acos(w)
+
+    if abs(w) == 1:
+        p = (1, 0, 0)   # bilo koji jedinicni
+    else:
+        xyz = np.array([q[0], q[1], q[2]])
+        norm = la.norm(xyz)
+
+        if norm != 0:
+            p = xyz / norm
+        else:
+            p = xyz
+
+    return p, fi
---
+++ src/test.py
@@ -0,0 +1,84 @@
+import math
+import numpy as np
+from functions import Euler2A
+from functions import A2AxisAngle
+from functions import Rodrigez
+from functions import A2Euler
+from functions import AngleAxis2Q
+from functions import Q2AxisAngle
+
+
+def test(fi, teta, psi):
+    print("Polazni Ojlerovi uglovi: ")
+    print("Fi ~ " + str(round(fi / math.pi * 180)) + " stepeni")
+    print("Teta ~ " + str(round(teta / math.pi * 180)) + " stepeni")
+    print("Psi ~ " + str(round(psi / math.pi * 180)) + " stepeni")
+    print()
+
+    A = Euler2A(fi, teta, psi)
+    print("Matrica A = Rz(psi)Ry(teta)Rx(fi):")
+    print(A)
+    print()
+
+    p, alpha = A2AxisAngle(A)
+    print("Rotacija oko prave p: ")
+    print(p)
+    print("za ugao: ")
+    print("Psi ~ " + str(round(alpha / math.pi * 180)) + " stepeni")
+    print()
+
+    B = Rodrigez(p, alpha)
+    print("Rodrigezovom formulom dobija se matrica: ")
+    print(B)
+    print()
+
+    beta, gamma, delta = A2Euler(B)
+    print("Uglovi dobijeni iz matrice dobijene Rodrigezovom formulom: ")
+    print("Beta ~ " + str(round(beta / math.pi * 180)) + " stepeni")
+    print("Gamma ~ " + str(round(gamma / math.pi * 180)) + " stepeni")
+    print("Delta ~ " + str(round(delta / math.pi * 180)) + " stepeni")
+    print()
+
+    q = AngleAxis2Q(p, alpha)
+    print("Kvaternion koji predstavlja rotaciju: (zaokruzen na 6 decimala)")
+    print([round(k, 6) for k in q])
+    print()
+
+    p, fi = Q2AxisAngle(q)
+    print("Kvaternion predstavlja rotaciju oko prave: ")
+    print(p)
+    print("za ugao:")
+    print("~ " + str(round(fi / math.pi * 180)) + " stepeni")
+    print()
+
+
+def main():
+    print("TEST SA SAJTA")
+    test(-math.atan(1 / 4), -math.asin(8 / 9), math.atan(4))
+    print()
+
+    print("TEST1")
+    A = np.array([[3, 1, math.sqrt(6)],
+                  [1, 3, -math.sqrt(6)],
+                  [-math.sqrt(6), math.sqrt(6), 2]])
+    A *= 0.25
+    fi, delta, psi = A2Euler(A)
+    test(fi, delta, psi)
+    print()
+
+    print("TEST2")
+    A = np.array([[0, 0, -1],
+                  [0, -1, 0],
+                  [-1, 0, 0]])
+    fi, delta, psi = A2Euler(A)
+
+    test(fi, delta, psi)
+    print()
+
+    print("TEST3")
+    test(0, math.pi/4, 0)
+    print()
+
+
+if __name__ == '__main__':
+    main()
